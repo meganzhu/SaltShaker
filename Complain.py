b710b4fdb3948966a28f4276b2184eecea6eee53
Megan Zhu <meganz@berkeley.edu>
2014-10-18 20:13:53 -0700

    WE dID A THING

---
+++ Complain.py
@@ -0,0 +1,94 @@
+class Complaint:
+	identity = 0
+	def __init__(self, content, identity = 0):
+		self.score = 0
+		self.content = content
+		self.identity = identity
+		Complaint.identity += 1
+
+	def __str__(self):
+		return "[{0},{1},{2}]".format(self.score, self.content, self.identity)
+
+	def salt_it(self):
+		"""
+		>>> com = Complaint("This sucks...")
+		>>> com.score
+		0
+		>>> com.content
+		'This sucks...'
+		>>> com.salt_it()
+		>>> com.score
+		1
+		"""
+		self.score += 1
+
+	def pepper_it(self):
+		"""
+		>>> com = Complaint("This sucks...")
+		>>> com.score
+		0
+		>>> com.content
+		'This sucks...'
+		>>> com.pepper_it()
+		>>> com.score
+		-1
+		"""
+		self.score -= 1
+
+	def get_content(self):
+		return self.content
+class Complaint_List:
+	def __init__(self):
+		"""
+		>>> com = Complaint("This sucks...")
+		>>> com2 = Complaint("This really sucks...")
+		>>> my_lst = Complaint_List([com, com2])
+		>>> com2.salt_it()
+		>>> com2.score
+		1
+		>>> my_lst.sort()
+		>>> str(my_lst)
+		"['[1,This really sucks...]', '[0,This sucks...]']"
+		"""
+		self.lst = []
+
+	def __str__(self):
+		if self.lst != []:
+			return str([str(each) for each in self.lst])
+		return "empty list!"
+
+	def add_complaint(self, string):
+		new_complaint = Complaint(string, Complaint.identity)
+		self.lst.append(new_complaint)
+
+	def sort(self):
+		def key(complaint):
+			return -complaint.score
+		self.lst.sort(key = key)
+
+	def get_complaints(self):
+		print('before sort')
+		self.print_list()
+		self.sort()
+		print('after sort')
+		self.print_list()
+		print('')
+		print('')
+		return self.lst
+
+	def salt(self, complaint_id):
+		for complaint in self.lst:
+			if complaint.identity == complaint_id:
+				complaint.salt_it()	
+
+	def pepper(self, complaint_id):
+		for complaint in self.lst:
+			if complaint.identity == complaint_id:
+				complaint.pepper_it()
+
+	def print_list(self):
+		for i in range(len(self.lst)):
+			print('element', i, 'is', self.lst[i].score)
+
+
+
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, g, render_template
-from SaltShaker import SaltShaker
+from Complain import Complaint_List
 import sqlite3
 
 complaints = Complaint_List()
@@ -26,21 +26,27 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     all_complaints = complaints.get_complaints()
-    return render_template('index.html', all_complaints)
+    return render_template('index.html', all_complaints = all_complaints)
 
-@app.route("/complaint", methods=["POST"])
+@app.route("/complain", methods=["POST"])
 def receive_complaint():
     complaints.add_complaint(request.form['complaint'])
-    return "Your saltiness is palpitable."
+    return home()
+    
 
 @app.route("/salt/<complaint_id>")
 def salt(complaint_id):
+    complaint_id = int(float(complaint_id))
     complaints.salt(complaint_id)
+    return home()
 
 @app.route("/pepper/<complaint_id>")
 def pepper(complaint_id):
+    complaint_id = int(float(complaint_id))
     complaints.pepper(complaint_id)
+    return home()
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
